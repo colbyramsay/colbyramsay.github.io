8d3687e8f8ab2cb145c9b46edd81bd74d8c088f5
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-03-19 04:49:52 -0700

    Add new sun animation

--- javascript.js
+++ javascript.js
@@ -1,4 +1,28 @@
+
+/*
+
+early attempt at "jock script" dark mode --
+it worked, but didn't maintain state.
+at the time, we didn't even know
+what a function was.
+
+--cr 3:18AM 3.19.24
+
+bc, canada
+
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-  }
+}
+
+*/
+
+const sun = document.getElementById("sun");
+const halo = document.getElementById("halo");
+
+const grow = () => {
+  sun.classList.add("grow");
+  halo.classList.add("grow-margin");
+}
+
+sun.addEventListener("click", grow);
